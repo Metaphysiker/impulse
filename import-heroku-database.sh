686803ab9516e9e0602479b46f952707b082e4c7
Metaphysiker <s.raess@me.com>
2021-02-02 19:56:23 +0100

    initial commit

--- import-heroku-database.sh
+++ import-heroku-database.sh
@@ -4,4 +4,4 @@ heroku pg:backups:download
 rails db:drop DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 rails db:create
 rails db:migrate
-pg_restore --verbose --clean --no-acl --no-owner -d tierethik_development latest.dump
+pg_restore --verbose --clean --no-acl --no-owner -d impulse_development latest.dump
