1c2ed47a5d31bf534eb97dafa58dbe34d4f1dc1e
Metaphysiker <s.raess@me.com>
2021-02-11 08:37:01 +0100

    first working mechaniz example

--- mechanize/google.rb
+++ mechanize/google.rb
@@ -1,18 +1,29 @@
 require 'rubygems'
 require 'mechanize'
-
-a = Mechanize.new { |agent|
-  agent.user_agent_alias = 'Mac Safari'
-}
-
+a = Mechanize.new
 a.get('https://impulsiv.herokuapp.com/') do |page|
+
+  exchange_page = a.click(page.link_with(:text => /Tauschbörse/))
+  #puts exchange_page.body.to_s
+  puts exchange_page.uri.to_s
+
   # Click the login link
   login_page = a.click(page.link_with(:text => /Login/))
 
   # Submit the login form
   my_page = login_page.form_with(:action => '/users/sign_in') do |f|
     #f.fields.each { |f| puts f.name }
-    f["email"]  = "s.raess@me.com"
-    f["password"]  = "abcdef"
+    f["user[email]"]  = "s.raess@me.com"
+    f["user[password]"]  = ""
   end.click_button
+  #byebug
+  #pp login_page
+
+  #puts my_page.body.to_s
+  puts my_page.uri.to_s
+  my_page.links.each do |link|
+    text = link.text.strip
+    next unless text.length > 0
+    puts text
+  end
 end
