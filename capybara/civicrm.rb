3ba51487a6e023bf9a082fadbe827c9100b75bde
Metaphysiker <s.raess@me.com>
2021-02-13 11:03:13 +0100

    mm mechaniz

---
+++ capybara/civicrm.rb
@@ -0,0 +1,24 @@
+require 'capybara'
+session = Capybara::Session.new(:selenium)
+session.visit "https://crm.impulse.swiss/user"
+
+session.within("#user-login") do
+  session.fill_in 'Benutzername', with: ''
+  session.fill_in 'Passwort', with: ''
+end
+session.click_button 'Anmelden'
+if session.has_content?("Fohrenbühlstrasse 4")
+  puts "Logged in"
+end
+#session.expect(page).to have_content 'Fohrenbühlstrasse 4'
+
+session.click_link('CiviCRM')
+
+abort("Not in CiviCRM Home") unless session.has_content?("CiviCRM Home")
+puts "In CiviCRM Home"
+#if session.has_content?("Arbeit")
+#  puts "All shiny, captain!"
+#else
+#  puts ":( no tagline fonud, possibly something's broken"
+#  exit(-1)
+#end
