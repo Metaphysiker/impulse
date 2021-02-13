1408638e8a9c00ef73237c1e4e0ba77c3269f579
Metaphysiker <s.raess@me.com>
2021-02-13 11:04:03 +0100

    mmmechaniz

--- capybara/civicrm_spec.rb
+++ capybara/civicrm_spec.rb
@@ -18,8 +18,8 @@ describe "the signin process", type: :feature, js: true do
   it "signs me in" do
     visit 'https://crm.impulse.swiss/user'
     within("#user-login") do
-      fill_in 'Benutzername', with: 'sara'
-      fill_in 'Passwort', with: 'sara1mpulze_BS253'
+      fill_in 'Benutzername', with: ''
+      fill_in 'Passwort', with: '
     end
     click_button 'Anmelden'
     expect(page).to have_content 'Fohrenbühlstrasse 4'
