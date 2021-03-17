0844b0f0d221464e46fde808e589b78911a18e4c
Metaphysiker <s.raess@me.com>
2021-03-17 08:29:10 +0100

    working on cv

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -45,38 +45,10 @@ class StaticPagesController < ApplicationController
 
   def cv_generator
     user = current_user #User.find(params[:id])
-    #current_month = I18n.t("date.month_names")[Date.today.month]
-    #current_year = Date.today.year
-    #older_name = "#{user.first_name} #{user.last_name} - #{user.recruitment_consultant} - #{user.disposal_period} - ALK #{user.unemployment_insurance_number}"
+    cv = generate_single_cv(user, Rails.root.join("public/cv/substantial-cv.odt"))
 
-    compressed_filestream = Zip::OutputStream.write_buffer do |zos|
-
-      range = user.disposal_period_start..user.disposal_period_end
-      end_of_month_dates = range.map(&:end_of_month).uniq
-
-      end_of_month_dates.each do |end_of_month_date|
-        month = I18n.t("date.month_names")[end_of_month_date.month]
-        year = end_of_month_date.year
-
-        cv = generate_single_cv(user, "amm_bescheinigung")
-
-        zos.put_next_entry "#{folder_name}/AMM/AMM-Bescheinigung_#{user.last_name}_#{month}_#{year}.odt"
-        zos.write cv.generate
-        #byebug
-      end
-
-      zos.put_next_entry "#{folder_name}/Matchingformular.odt"
-      matchingformular = File.open("odf-templates/Matchingformular.odt")
-      zos.write matchingformular.read
-      matchingformular.close
-      zos.put_next_entry "#{folder_name}/Stammblatt_#{user.last_name}.ods"
-      zos.write stammblatt.generate
-      zos.put_next_entry "#{folder_name}/Mentoring_50+_Kundenzufriedenheit_#{user.last_name}.odt"
-      zos.write kundenzufriedenheit.generate
-    end
-
-    compressed_filestream.rewind
-    send_data compressed_filestream.read, filename: "#{folder_name}.zip"
+    #compressed_filestream.rewind
+    send_data cv.generate, filename: "cv.odt"
     #send_data zippy.read, filename: "zippy.zip"
     #send_data stammblatt.generate, filename: "AMM-Bescheinigung_#{user.last_name}_#{current_month}_#{current_year}.odt"
     #send_data stammblatt.generate, filename: "AMM-Bescheinigung_#{user.last_name}_#{current_month}_#{current_year}.ods"
@@ -84,9 +56,7 @@ class StaticPagesController < ApplicationController
   end
 
   def generate_single_cv(user, file_path)
-
     #attribute_array = ["last_name", "first_name"]
-
     cv = ODFReport::Report.new(file_path) do |r|
       User.showable_attribute_names_for_cv.each do |attribute|
         r.add_field attribute.to_sym, user.public_send(attribute)
