648dfbc8c919946e036f8812cbd6bb4fef89562e
Metaphysiker <s.raess@me.com>
2021-04-14 08:12:58 +0200

    working on tests

--- cypress/integration/examples/support_request.spec.js
+++ cypress/integration/examples/support_request.spec.js
@@ -28,20 +28,24 @@ context('basic test with login', () => {
 
   // https://on.cypress.io/interacting-with-elements
 
-  it('visits index and creates and deletes support request', () => {
+  it('visits index and creates, edits and deletes support request', () => {
 
     cy.visit('localhost:3000')
     //cy.contains(model_name).click()
     cy.get("[data-cy=" + model_name + "_index]").contains(model_name_singular_german).click()
     cy.get('h1').contains(model_name_singular_german)
     cy.get("[data-cy=create-" + model_name + "]").contains(model_name_singular_german).click()
-    cy.get("[data-cy=new-" + model_name + "]").within(($form) => {
+    cy.get("[data-cy=form-" + model_name + "]").within(($form) => {
       cy.get('#' + model_name + '_title').type(lorem_ipsum_title)
       cy.get('#' + model_name + '_content').type(lorem_ipsum_content)
-      cy.get('#' + model_name + '_area_of_expertise').select(area_of_expertise)
+      //cy.get('#' + model_name + '_area_of_expertise').select(area_of_expertise)
       cy.root().submit()
     })
 
+    cy.contains(model_name_singular_german + " wurde erstellt!")
+    cy.get("." + model_name + "_card").contains(lorem_ipsum_title)
+    cy.get("." + model_name + "_card").contains(lorem_ipsum_content)
+    cy.get("[data-cy=edit-" + model_name + "]").contains(model_name_singular_german).click()
     // https://on.cypress.io/type
     //cy.get('body').should('have.value', 'Tauschbörse')
     //cy.get('body').contains('Tauschbörse')
