aa29d7ea9dc9f5dfe30496ac1c2c7e48a295d129
Metaphysiker <s.raess@me.com>
2021-04-14 07:43:56 +0200

    working on tests

---
+++ cypress/integration/examples/support_request.spec.js
@@ -0,0 +1,67 @@
+/// <reference types="cypress" />
+//const requiredExample = require('../../fixtures/user')
+//import user from '/cypress/fixtures/user.json'
+const model_name_singular_german = "Call for Help"
+const model_name_plural_german = "Calls for Help"
+const model_name = "support_request"
+
+const lorem_ipsum_title = "Lorem Ipsum"
+const lorem_ipsum_content = "Lorem Ipsum Content"
+const area_of_expertise = "Lebenslauf-Gestaltung"
+
+context('basic test with login', () => {
+  beforeEach(() => {
+    let user;
+    cy.request('https://randomuser.me/api/?results=10').then(
+      (response) => {
+        cy.log(response.body["results"][0]["name"]["first"])
+        // response.body is automatically serialized into JSON
+        //expect(response.body).to.have.property('name', 'Jane') // true
+        user = {email: response.body["results"][0]["email"], first_name: response.body["results"][0]["name"]["first"], last_name: response.body["results"][0]["name"]["last"]};
+        cy.wrap(user).as('user')
+        // other test code here
+        cy.login(user["email"], user["first_name"], user["last_name"], "password")
+      }
+    )
+
+  })
+
+  // https://on.cypress.io/interacting-with-elements
+
+  it('visits index and creates and deletes support request', () => {
+
+    cy.visit('localhost:3000')
+    //cy.contains(model_name).click()
+    cy.get("[data-cy=" + model_name + "_index]").contains(model_name_singular_german).click()
+    cy.get('h1').contains(model_name_singular_german)
+    cy.get("[data-cy=create-" + model_name + "]").contains(model_name_singular_german).click()
+    cy.get("[data-cy=new-" + model_name + "]").within(($form) => {
+      cy.get('#' + model_name + '_title').type(lorem_ipsum_title)
+      cy.get('#' + model_name + '_content').type(lorem_ipsum_content)
+      cy.get('#' + model_name + '_area_of_expertise').select(area_of_expertise)
+      cy.root().submit()
+    })
+
+    // https://on.cypress.io/type
+    //cy.get('body').should('have.value', 'Tauschbörse')
+    //cy.get('body').contains('Tauschbörse')
+
+    //cy.get('@user').then(user => {
+    //  expect(user["first_name"]).to.contain("Jonas")
+    //})
+  })
+
+  it('visits mein Impulse', () => {
+    // https://on.cypress.io/type
+    //cy.get('body').should('have.value', 'Tauschbörse')
+    cy.contains('Mein Impulse').click()
+    cy.contains('Mein Impulse')
+    cy.get('@user').then(user => {
+      cy.contains(user["first_name"])
+      cy.contains(user["last_name"])
+      cy.contains(user["email"])
+    })
+    //cy.get('body').contains(first_name)
+  })
+
+})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -14,36 +14,17 @@ import 'cypress-file-upload';
 Cypress.Commands.add('login', (email, first_name, last_name, password) => {
   cy.visit('localhost:3000')
   //cy.get('body').contains('Tauschbörse')
+
   cy.get('.form_for_user').within(($form) => {
-  // you have access to the found form via
-  // the jQuery object $form if you need it
-
-  // cy.get() will only search for elements within form,
-  // not within the entire document
-  cy.get('#user_email').type(email)
-  cy.get('#user_first_name').type(first_name)
-  cy.get('#user_last_name').type(last_name)
-  cy.get('#user_password').type(password)
-
-  //var img1 = document.createElement("img");
-  //img1.src = "https://picsum.photos/200/300;
-  //var img = new Image();
-  //img.src = "https://www.google.com/images/srpr/logo4w.png"
-  //const blob = Cypress.Blob.base64StringToBlob('https://picsum.photos/200/300', 'image/png')
-  //cy.fixture('https://picsum.photos/200/300').as('logo')
-  //cy.fixture('images/logo.png').as('logo')
-  const fixtureFile = 'images/test.jpg';
-  //const fixtureFile = cy.request("https://picsum.photos/200/300").body
-  cy.get('#user_cover').attachFile(fixtureFile);
-
-
-  cy.root().submit()
-  //cy.get('.alert-info').should('be.visible')
-  //cy.wait(3000);
-  //cy.get('body').should('have.value', "Tauschbörse")
-
-  //cy.get('body').contains('Account wurde erstellt!')
+    cy.get('#user_email').type(email)
+    cy.get('#user_first_name').type(first_name)
+    cy.get('#user_last_name').type(last_name)
+    cy.get('#user_password').type(password)
+    const fixtureFile = 'images/stalin_portrait.jpg';
+    cy.get('#user_cover').attachFile(fixtureFile);
+    cy.root().submit()
   })
+
   cy.wait(1000)
   cy.contains('Account wurde erstellt!')
 })
