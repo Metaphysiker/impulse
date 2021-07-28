3cde6197b50f68e8fc529a06ad66712099dd9ac5
Metaphysiker <s.raess@me.com>
2021-07-28 07:00:31 +0200

    added seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_27_131334) do
+ActiveRecord::Schema.define(version: 2021_07_27_145323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,6 +88,9 @@ ActiveRecord::Schema.define(version: 2021_07_27_131334) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.string "location", default: ""
+    t.string "company", default: ""
+    t.string "job", default: ""
     t.index ["user_id"], name: "index_cv_units_on_user_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -36,24 +36,30 @@ testuser = User.create(email: "test@gmail.com", password: "abcdef",
   disposal_period_start: Date.today, disposal_period_end: Date.today + 3.months,
   personal_number: "4626262555", unemployment_insurance_number: "12",
   job_title: "Ausbildungsberaterin", website: "www.my-website.com",
+  nationality: "Schweiz",
+  marital_status: "ledig",
   short_description: "Ich bin ausgebildete Lehrerin und arbeite als Bildungsberaterin. Ich suche eine Stelle mit der Möglichkeit, Einfluss auf kleine und grössere Bildungssysteme zu nehmen.")
 
 #testuser = User.find_by_email("test@gmail.com")
 
 testuser.cv_units << CvUnit.create(
-  category: "experience", name: "Abteilungsleiterin - Zentrum für Bildung",
+  category: "experience", name: "Abteilungsleiterin",
+  company: "Zentrum für Bildung", location: "Bern",
   content: "Leitung der verschiedenen Teams der Abteilung für Erwachsenenbildung \nBeurteilung und Einstellung von Personal \nGenehmigung von Budgets für spezielle Bildungsprojekte", start_date: Date.today - 1.year, end_date: Date.today)
 
 testuser.cv_units << CvUnit.create(
-  category: "experience", name: "Abteilungsspezialistin - Forschungszentrum Biel",
+  category: "experience", name: "Abteilungsspezialistin",
+  company: "Forschungszentrum", location: "Biel",
   content: "Leitung von Workshops für Erwachsenenbildung und Lerntheorien \nErstellung und Einführung von neuen Lernstrategien für Erwachsene \nMentoring neuer Lehrpersonen", start_date: Date.today - 2.year, end_date: Date.today - 1.year)
 
 testuser.cv_units << CvUnit.create(
-  category: "education", name: "MSC in Psychologie - Universität Frankfurt",
+  category: "education", name: "MSC in Psychologie",
+  company: "Goethe-Universität", location: "Frankfurt",
   content: "Thema der Masterarbeit: Neue Ansätze für spezialisierte Erwachsenenbildung und -training\nVorstand im Debattierclub", start_date: Date.today - 3.year, end_date: Date.today - 2.year)
 
 testuser.cv_units << CvUnit.create(
-  category: "education", name: "BSC in Psychologie - Universität Groningen",
+  category: "education", name: "BSC in Psychologie",
+  company: "Fachhochschule Graubünden", location: "Chur",
   content: "Thema der Bachelorarbeit: Können Kinder besser lernen als Erwachsene?", start_date: Date.today - 5.year, end_date: Date.today - 4.year)
 
 testuser.cv_units << CvUnit.create(
