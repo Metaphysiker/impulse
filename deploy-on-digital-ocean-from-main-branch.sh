d62288a629f0c5fae09ee0a3807771d3a47e75c6
Metaphysiker <s.raess@me.com>
2022-04-16 10:44:27 +0200

    added script

---
+++ deploy-on-digital-ocean-from-main-branch.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo start
+RAILS_ENV=production rails assets:clobber
+RAILS_ENV=production rails assets:precompile
+git add .
+git commit -m "precompile"
+git push origin main
+
+ssh sandro@159.65.120.231 << EOF
+  echo ssh sandro
+  cd impulse
+  git stash
+  git pull origin main
+  bundle config set --local without 'development test'
+  bundle install
+  RAILS_ENV=production rails db:migrate
+  RAILS_ENV=production rails assets:clobber
+  RAILS_ENV=production rails assets:precompile
+EOF
+
+ssh root@159.65.120.231 << EOF
+  echo ssh root
+  cd /home/sandro/impulse/
+  sudo service nginx restart
+EOF
