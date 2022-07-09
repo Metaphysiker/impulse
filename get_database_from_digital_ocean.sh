b01ba8b4be63173b22eed358f238e238114d2ecd
Metaphysiker <s.raess@me.com>
2022-07-09 18:38:00 +0200

    tests pass

---
+++ get_database_from_digital_ocean.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+ssh sandro@159.65.120.231 << EOF
+  cd impulse
+  pg_dump impulse_production > latest.sql
+EOF
+
+scp sandro@159.65.120.231:/home/sandro/impulse/latest.sql latest.sql
+
+rails db:drop DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+rails db:create
+rails db:migrate
+
+psql impulse_development < latest.sql
